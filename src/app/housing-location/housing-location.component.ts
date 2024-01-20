cfcbdbce4458ea1d0c6974a5ce078b23595b375b
iykechuks11 <johnchamp1111@gmail.com>
2024-01-20 22:26:31 +0300

    Created an interface that created a new data type for my app. Created a new component, "detail" Added styling

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
+  @Input() housingLocation!:HousingLocation; // The ! is called the non-null ascertion operator, it tells the TS compiler that the value of this property won't be null or undefined.
 
 }
